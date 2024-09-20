01da980669e47610d196b2274a805d0d59d4ebde
AirHolic <2670446215@qq.com>
2024-09-20 16:17:40 +0800

    mcuflash针对boot进行优化

--- Tools/Inc/mcu_flash.h
+++ Tools/Inc/mcu_flash.h
@@ -1,9 +1,11 @@
 #ifndef __MCU_FLASH_H__
 #define __MCU_FLASH_H__
 
+#include "main.h"
+
 #define FLASH_START_ADDR 0x08000000
 #define FLASH_TYPE FLASH_PAGE
-#define FLASH_TOTAL_NUM 128
+#define FLASH_TOTAL_NUM 127
 #define LARGE_FLASH
 
 #if (FLASH_TYPE == FLASH_PAGE)
@@ -14,15 +16,17 @@
 
 #ifdef LARGE_FLASH
 #define FLASH_SIZE 2048
+//#define FLASH_PAGE_SIZE 0x800U
 #endif
 
-#define FLASH_PAGEx(x) (FLASH_START_ADDR + (FLASH_SIZE * x))//x<FLASH_TOTAL_NUM
+#define FLASH_PAGEx(x) (FLASH_START_ADDR+FLASH_SIZE*(x))//x < FLASH_TOTAL_NUM
 
 #endif
 
-
-
+void mcu_flash_erase(uint32_t addr, uint32_t len);
 void mcu_flash_write(uint32_t addr, uint32_t *data, uint32_t size);
+void mcu_flash_nocheck_write(uint32_t addr, uint32_t *data, uint32_t size);
+void mcu_uint8_flash_write(uint32_t addr, uint8_t *data, uint32_t size);
 void mcu_flash_read(uint32_t addr, uint32_t *data, uint32_t size);
 
 #endif /* _MCU_FLASH_H__ */
--- Tools/Src/mcu_flash.c
+++ Tools/Src/mcu_flash.c
@@ -1,41 +1,115 @@
+//#include "main.h"
 #include "mcu_flash.h"
-#include "main.h"
+
+#define FLASH_FLAG_ALL_ERRORS FLASH_FLAG_BSY | FLASH_FLAG_EOP | \
+    FLASH_FLAG_PGERR | FLASH_FLAG_WRPERR | FLASH_FLAG_OPTVERR 
+
+static uint32_t data32[256];
+
+/**
+ * @brief Erase flash.
+ * @param addr The address of the flash.
+ * @param len The length of the flash.
+ */
+void mcu_flash_erase(uint32_t addr, uint32_t len)
+{
+    #if RTOS == 1
+    taskENTER_CRITICAL();
+    #endif
+    uint32_t page_error = 0;
+    FLASH_EraseInitTypeDef EraseInitStruct;
+    EraseInitStruct.TypeErase = FLASH_TYPEERASE_PAGES;
+    EraseInitStruct.Banks = FLASH_BANK_1;
+    EraseInitStruct.PageAddress = addr;
+    EraseInitStruct.NbPages = len / FLASH_SIZE + 1;
+    //__HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_ALL_ERRORS);
+    if(HAL_FLASH_Unlock() != HAL_OK)
+    {
+        return;
+    }
+    //if(HAL_FLASHEx_Erase_IT(&EraseInitStruct) != HAL_OK)
+    if(HAL_FLASHEx_Erase(&EraseInitStruct, &page_error) != HAL_OK)
+    {
+        HAL_FLASH_Lock();
+        return;
+    }
+    HAL_FLASH_Lock();
+    #if RTOS == 1
+    taskEXIT_CRITICAL();
+    #endif
+}
 
 /**
  * @brief Write data to flash.
  * @param addr The address of the flash.
  * @param data The data to be written.
- * @param len The length of the data.
+ * @param len The length of the data.Less than or equal to FLASH_SIZE.
  */
 void mcu_flash_write(uint32_t addr, uint32_t *data, uint32_t len)
 {
-    uint32_t *PageError;
+    mcu_flash_erase(addr, len);
+    #if RTOS == 1
+    taskENTER_CRITICAL();
+    #endif
     if(HAL_FLASH_Unlock() != HAL_OK)
     {
         return;
     }
-    FLASH_EraseInitTypeDef EraseInitStruct;
-    EraseInitStruct.TypeErase = FLASH_TYPEERASE_PAGES;
-    EraseInitStruct.Banks = FLASH_BANK_1;
-    EraseInitStruct.PageAddress = addr;
-    EraseInitStruct.NbPages = 1;
-    if(HAL_FLASHEx_Erase(&EraseInitStruct, PageError) != HAL_OK)
-    {
-        printf("Erase failed: %#X\r\n", *PageError);
-        HAL_FLASH_Lock();
-        return;
-    }
-
     for(uint32_t i = 0; i < len; i++)
     {
         if(HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, addr + i * 4, data[i]) != HAL_OK)
         {
-            printf("Write failed: %#X\r\n", addr + i * 4);
             HAL_FLASH_Lock();
             return;
         }
     }
     HAL_FLASH_Lock();
+    #if RTOS == 1
+    taskEXIT_CRITICAL();
+    #endif
+}
+
+/**
+ * @brief Write data to flash without checking.
+ * @param addr The address of the flash.
+ * @param data The data to be written.
+ * @param len The length of the data.Less than or equal to FLASH_SIZE.
+ */
+void mcu_flash_nocheck_write(uint32_t addr, uint32_t *data, uint32_t len)
+{
+    #if RTOS == 1
+    taskENTER_CRITICAL();
+    #endif
+    if(HAL_FLASH_Unlock() != HAL_OK)
+    {
+        return;
+    }
+    for(uint32_t i = 0; i < len; i++)
+    {
+        if(HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, addr + i * 4, data[i]) != HAL_OK)
+        {
+            HAL_FLASH_Lock();
+            return;
+        }
+    }
+    HAL_FLASH_Lock();
+    #if RTOS == 1
+    taskEXIT_CRITICAL();
+    #endif
+}
+
+/**
+ * @brief Write uint8_t data to flash.
+*/
+void mcu_uint8_flash_write(uint32_t addr, uint8_t *data, uint32_t size)
+{
+    uint32_t len = size / 4 + size % 4;
+    
+    for(uint32_t i = 0; i < len; i++)
+    {
+        data32[i] = data[i * 4] | data[i * 4 + 1] << 8 | data[i * 4 + 2] << 16 | data[i * 4 + 3] << 24;
+    }
+    mcu_flash_nocheck_write(addr, data32, len);
 }
 
 /**
